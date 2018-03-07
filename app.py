801538c8267adb1de8431fa7aeaaa6a4fcc74501
sanair96 <sannair96@gmail.com>
2018-03-07 13:08:48 +0530

    changes in all templates,  added base  template, added app routes for index, login, signup

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask,render_template,Response
+app = Flask(__name__)
+
 @app.route('/')
+@app.route('/index')
 def start():
-    return "hello world!"
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+	return  render_template("login.html")
+
+@app.route('/signup')
+def signup():
+	return render_template("signup.html")
+
+@app.route('/logout')
+def logout():
+	return "logout"
 
 if __name__ == '__main__':
  	app.run()
